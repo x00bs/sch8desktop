73d595736d4fd62a36930efc9ef81f42e46d5999
Kirill Novik <kirill.novik.12@gmail.com>
2018-07-19 17:25:29 +0700

    obs

--- terminal/circle/index.php
+++ terminal/circle/index.php
@@ -8,16 +8,28 @@
 
 <body>
  <?php include_once ("../topmenu.htm")?>
-<table border=1 width= 95% align=center cellpadding=10>
+<table width= 95% align=center cellpadding=10>
  <tr>
-  <td width=20%>
+  <td width=20% valign=top>
   <div id="CircleLeftMenu"></div>
  <!--  <a href="#" class="side-nav-but">Кружки, секции</a>
     <a href="#" class="side-nav-but">Сайт школы</a>
 	-->
   </td>
   <td>
-  <div id="MainText"></div>
+  <div id="MainText">
+  <?php
+  if(isset($_GET["page"]))
+  {	
+	$page=$_GET["page"];
+	include_once("info/$page.htm");
+  }
+  else
+  {
+	include_once("info/main.htm");
+  }
+  ?>
+  </div>
   </td>
  </tr>
 </table>
@@ -26,19 +38,20 @@
 <footer></footer>
 <script>
     var myCircles = { 
-      "karate": "Каратэ"
-     ,"skalolaz": "Скалолазание"
-	 ,"basket": "Баскетбол"}
+      "breakdance": "Брейк-данс"
+     ,"football": "Футбол"
+	 ,"basketball": "Баскетбол"
+	 ,"pankration": "Панкратион"}
 
   $.each(myCircles, function( index, value ) {
-   $("<a class='side-nav-but' href='' id='"+index+"'>"+value+"</a>").appendTo("#CircleLeftMenu");
+   $("<a class='side-nav-but' href='?page="+index+"' id='"+index+"'>"+value+"</a>").appendTo("#CircleLeftMenu");
    }) 
        
 
    $( ".side-nav-but" ).click(function() {
-   alert( "/info/"+$(this).attr("id")+".htm");
+//   alert( "/info/"+$(this).attr("id")+".htm");
   // $("<b>text</b>").appendTo("#MainText");
-	$(location).attr("href","#"+$(this).attr("id"));
+//	$(location).attr("href","#"+$(this).attr("id"));
 	//	$( "#MainText" ).html("/info/"+$(this).attr("id")+".htm")
 	//$( "#MainText" ).text("<b>LOL</b>");
    });
